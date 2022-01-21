a877deaacaf39e3b3a5ef8010e6d3f002191a1f7
PhyXTGears <phyxtgearspro@gmail.com>
2022-01-20 21:40:20 -0500

    intake: add methods to control intake position
    - extenIntake(): Work in progress (wip).  Ramp up position motor speed
      to target speed.  No consideration for whether target position is
      reached.  That is the responsibility of commands.
    
    - retractIntake(): wip. Also ramp up position motor speed to target
      speed.  Commands will monitor distance to target position.
    
    - isIntakeExtended()
    
    - getIntakePosition()
    
    - getDistanceToPosition(): Report how far away from final position the
      intake is.  Returns near zero when at final position.
    
    - Alex G.

--- src/main/cpp/intake/intake.cpp
+++ src/main/cpp/intake/intake.cpp
@@ -1,9 +1,37 @@
 #include "intake/intake.h"
 
+const double kDeploySpeedFactor = 0.1;
+
+void Intake::extendIntake () {
+    if (mDeployTargetSpeed != mDeployCurrentSpeed) { // add a tenth of the difference to current_speed
+        mDeployCurrentSpeed += kDeploySpeedFactor * (mDeployTargetSpeed - mDeployCurrentSpeed);
+    }
+    mDeployMotor.Set(ControlMode::PercentOutput, mDeployCurrentSpeed);
+}
+
+void Intake::retractIntake () {
+    if (-mDeployTargetSpeed != mDeployCurrentSpeed) { // add a tenth of the difference to current_speed
+        mDeployCurrentSpeed += kDeploySpeedFactor * (mDeployTargetSpeed - mDeployCurrentSpeed);
+    }
+    mDeployMotor.Set(ControlMode::PercentOutput, mDeployCurrentSpeed);
+}
+
 void Intake::runRollers (double speed) {
     mRollerMotor.Set(ControlMode::PercentOutput, speed);
 }
 
 void Intake::stopRollers () {
     runRollers(0.0);
+}
+
+bool Intake::isIntakeExtended () {
+    return mIsIntakeExtended;
+}
+
+double Intake::getIntakePosition () {
+    return mIntakePosition.Get();
+}
+
+double Intake::getDistanceToPosition (double targetPosition) {
+    return targetPosition - getIntakePosition();
 }
--- src/main/include/intake/intake.h
+++ src/main/include/intake/intake.h
@@ -6,12 +6,29 @@
 
 class Intake : public frc2::SubsystemBase {
 public:
+    void extendIntake ();
+    void retractIntake ();
+
     void runRollers (double speed);
     void stopRollers ();
+
+    bool isIntakeExtended ();
+
+    double getIntakePosition ();
+
+    double getDistanceToPosition (double targetPosition);
 
 private:
     TalonSRX mRollerMotor {0}; // the motor that turns the rollers to pull the ball in
     TalonSRX mDeployMotor {0}; // the motor that flips the intake in/out
 
     frc::AnalogPotentiometer mIntakePosition {0, 360.0, 0.0}; // {channel, full range, offset}
+
+    bool mIsIntakeExtended = false;
+
+    double mDeployTargetSpeed = 1.0;
+    double mDeployCurrentSpeed = 0.0;
+
+    double mIntakeRetractedPosition = 0.0; // value the potentiometer should read when retracted
+    double mIntakeExtendedPosition = 90.0; // value the potentiometer should read when extended
 };
