a2ce4fdebf681cace368e39250c4be02e5923f5f
PhyXTGears <phyxtgearspro@gmail.com>
2022-01-25 18:36:28 -0500

    Add RetractIntake command
    - Alex G.

---
+++ src/main/cpp/commands/RetractIntake.cpp
@@ -0,0 +1,22 @@
+#include "commands/RetractIntake.h"
+
+const double kAcceptablePositionError = 0.01; // buffer value for potentiometer position
+
+RetractIntakeCommand::RetractIntakeCommand (Intake* intake) {
+    AddRequirements(intake);
+    mIntake = intake;
+}
+
+void RetractIntakeCommand::Initialize () {
+    mIntake->retractIntake();
+}
+
+void RetractIntakeCommand::Execute () {}
+
+void RetractIntakeCommand::End (bool interrupted) {
+    mIntake->setStationary(false);
+}
+
+bool RetractIntakeCommand::IsFinished () {
+    return abs(mIntake->getDistanceToPosition()) < kAcceptablePositionError;
+}
