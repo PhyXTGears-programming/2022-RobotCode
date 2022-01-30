ba6716d1e6fc4808e1458a3a3ebdfb415535d718
nickquednow <56178732+nickquednow@users.noreply.github.com>
2022-01-30 14:49:33 -0500

    initialise PID controllers and values

--- src/main/cpp/drivetrain/drivetrain.cpp
+++ src/main/cpp/drivetrain/drivetrain.cpp
@@ -23,6 +23,29 @@ double gyroFieldAngle = 0;
 Drivetrain::Drivetrain()
 {
     tables.LogToNetworktable("Drivetrain initialised");
+
+    //initial drive motor PID values
+    Drivetrain::setPidValues(m_PID_DriveMotor1, k_PID_DriveMotor1_P, k_PID_DriveMotor1_I, k_PID_DriveMotor1_D, k_PID_DriveMotor1_F, k_minOutput, k_MaxOutput);
+    Drivetrain::setPidValues(m_PID_DriveMotor2, k_PID_DriveMotor2_P, k_PID_DriveMotor2_I, k_PID_DriveMotor2_D, k_PID_DriveMotor2_F, k_minOutput, k_MaxOutput);
+    Drivetrain::setPidValues(m_PID_DriveMotor3, k_PID_DriveMotor3_P, k_PID_DriveMotor3_I, k_PID_DriveMotor3_D, k_PID_DriveMotor3_F, k_minOutput, k_MaxOutput);
+    Drivetrain::setPidValues(m_PID_DriveMotor4, k_PID_DriveMotor4_P, k_PID_DriveMotor4_I, k_PID_DriveMotor4_D, k_PID_DriveMotor4_F, k_minOutput, k_MaxOutput);
+    //initial steering PID values
+    Drivetrain::setPidValues(m_PID_SteerMotor1, k_PID_SteerMotor1_P, k_PID_SteerMotor1_I, k_PID_SteerMotor1_D, k_PID_SteerMotor1_F, k_minOutput, k_MaxOutput);
+    Drivetrain::setPidValues(m_PID_SteerMotor2, k_PID_SteerMotor2_P, k_PID_SteerMotor2_I, k_PID_SteerMotor2_D, k_PID_SteerMotor2_F, k_minOutput, k_MaxOutput);
+    Drivetrain::setPidValues(m_PID_SteerMotor3, k_PID_SteerMotor3_P, k_PID_SteerMotor3_I, k_PID_SteerMotor3_D, k_PID_SteerMotor3_F, k_minOutput, k_MaxOutput);
+    Drivetrain::setPidValues(m_PID_SteerMotor4, k_PID_SteerMotor4_P, k_PID_SteerMotor4_I, k_PID_SteerMotor4_D, k_PID_SteerMotor4_F, k_minOutput, k_MaxOutput);
+}
+
+void Drivetrain::setPidValues(rev::SparkMaxPIDController PIDController, double k_P,
+                              double k_I, double k_D, double k_FF,
+                              double k_minValue, double k_maxValue, double k_IZone /*= 0.0*/)
+{
+    PIDController.SetP(k_P);
+    PIDController.SetI(k_I);
+    PIDController.SetD(k_D);
+    PIDController.SetFF(k_FF);
+    PIDController.SetOutputRange(k_minValue, k_maxValue);
+    PIDController.SetIZone(k_IZone);
 }
 
 //set the heading of the robot as a whole, and set the individual wheels to the correct direction.
--- src/main/include/drivetrain/drivetrain.h
+++ src/main/include/drivetrain/drivetrain.h
@@ -119,6 +119,22 @@ private:
      //gets the angle of the drive modules in radians (from -pi to +pi)
      std::vector<double> getWheelDirection(double speed, double angle, double clockwiseSpin, double centerFieldAngle);
 
+     /**
+     * @brief Set the PID Values of the motor
+     * 
+     * @param PIDController the motor controller PID controller object
+     * @param k_P the P value to set it to
+     * @param k_I the I value to set it to
+     * @param k_D the D value to set it to
+     * @param k_FF the Feed foreward value to set it to
+     * @param k_minValue the minimum output value of the PID loop
+     * @param k_maxValue the miximum output value of the PID loop
+     * @param k_IZone the I zone value to set it to (defaults to 0)
+     */
+     void setPidValues(rev::SparkMaxPIDController PIDController, double k_P, double k_I, double k_D, double k_FF, double k_minValue, double k_maxValue, double k_IZone = 0.0);
+
+     //initialised the motors
+
      rev::CANSparkMax m_DriveMotor1{interfaces::k_Drive1, rev::CANSparkMax::MotorType::kBrushless};
      rev::CANSparkMax m_DriveMotor2{interfaces::k_Drive2, rev::CANSparkMax::MotorType::kBrushless};
      rev::CANSparkMax m_DriveMotor3{interfaces::k_Drive3, rev::CANSparkMax::MotorType::kBrushless};
@@ -127,5 +143,61 @@ private:
      rev::CANSparkMax m_SteerMotor2{interfaces::k_Steer2, rev::CANSparkMax::MotorType::kBrushless};
      rev::CANSparkMax m_SteerMotor3{interfaces::k_Steer3, rev::CANSparkMax::MotorType::kBrushless};
      rev::CANSparkMax m_SteerMotor4{interfaces::k_Steer4, rev::CANSparkMax::MotorType::kBrushless};
+
+     //initialised PID controls
+
+     rev::SparkMaxPIDController m_PID_DriveMotor1 = Drivetrain::m_DriveMotor1.GetPIDController();
+     rev::SparkMaxPIDController m_PID_DriveMotor2 = Drivetrain::m_DriveMotor2.GetPIDController();
+     rev::SparkMaxPIDController m_PID_DriveMotor3 = Drivetrain::m_DriveMotor3.GetPIDController();
+     rev::SparkMaxPIDController m_PID_DriveMotor4 = Drivetrain::m_DriveMotor4.GetPIDController();
+     rev::SparkMaxPIDController m_PID_SteerMotor1 = Drivetrain::m_SteerMotor1.GetPIDController();
+     rev::SparkMaxPIDController m_PID_SteerMotor2 = Drivetrain::m_SteerMotor2.GetPIDController();
+     rev::SparkMaxPIDController m_PID_SteerMotor3 = Drivetrain::m_SteerMotor3.GetPIDController();
+     rev::SparkMaxPIDController m_PID_SteerMotor4 = Drivetrain::m_SteerMotor4.GetPIDController();
+
+     //global constants for the motors
+     const double k_MaxOutput = 1.0;
+     const double k_minOutput = -1.0;
+
+     //hardcoded PID values (temporary)
+     const double k_PID_DriveMotor1_P = 0.0;
+     const double k_PID_DriveMotor1_I = 0.0;
+     const double k_PID_DriveMotor1_D = 0.0;
+     const double k_PID_DriveMotor1_F = 0.0;
+
+     const double k_PID_DriveMotor2_P = 0.0;
+     const double k_PID_DriveMotor2_I = 0.0;
+     const double k_PID_DriveMotor2_D = 0.0;
+     const double k_PID_DriveMotor2_F = 0.0;
+
+     const double k_PID_DriveMotor3_P = 0.0;
+     const double k_PID_DriveMotor3_I = 0.0;
+     const double k_PID_DriveMotor3_D = 0.0;
+     const double k_PID_DriveMotor3_F = 0.0;
+
+     const double k_PID_DriveMotor4_P = 0.0;
+     const double k_PID_DriveMotor4_I = 0.0;
+     const double k_PID_DriveMotor4_D = 0.0;
+     const double k_PID_DriveMotor4_F = 0.0;
+
+     const double k_PID_SteerMotor1_P = 0.0;
+     const double k_PID_SteerMotor1_I = 0.0;
+     const double k_PID_SteerMotor1_D = 0.0;
+     const double k_PID_SteerMotor1_F = 0.0;
+
+     const double k_PID_SteerMotor2_P = 0.0;
+     const double k_PID_SteerMotor2_I = 0.0;
+     const double k_PID_SteerMotor2_D = 0.0;
+     const double k_PID_SteerMotor2_F = 0.0;
+
+     const double k_PID_SteerMotor3_P = 0.0;
+     const double k_PID_SteerMotor3_I = 0.0;
+     const double k_PID_SteerMotor3_D = 0.0;
+     const double k_PID_SteerMotor3_F = 0.0;
+
+     const double k_PID_SteerMotor4_P = 0.0;
+     const double k_PID_SteerMotor4_I = 0.0;
+     const double k_PID_SteerMotor4_D = 0.0;
+     const double k_PID_SteerMotor4_F = 0.0;
 
 };
