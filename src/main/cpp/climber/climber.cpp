7e2efc853a782e1674742b1bf1c10492f154f461
PhyXTGears <phyxtgearspro@gmail.com>
2022-02-17 21:14:35 -0500

    Add basic methods for climber
    Also added setpoints to config.toml
    - Alex G.

--- src/main/cpp/climber/climber.cpp
+++ src/main/cpp/climber/climber.cpp
@@ -1,2 +1,51 @@
 #include "climber/climber.h"
 
+Climber::Climber(std::shared_ptr<cpptoml::table> toml) {
+    config.lockServoPosition = toml->get_qualified_as<double>("lockServoPosition").value_or(0.0);
+    config.extendSpeed = toml->get_qualified_as<double>("extendSpeed").value_or(0.0);
+    config.retractSpeed = toml->get_qualified_as<double>("retractSpeed").value_or(0.0);
+    config.friction.innerStaticFriction = toml->get_qualified_as<double>("innerStaticFriction").value_or(0.0);
+    config.friction.outerStaticFriction = toml->get_qualified_as<double>("outerStaticFriction").value_or(0.0);
+    config.friction.innerStaticFrictionWithLoad = toml->get_qualified_as<double>("innerStaticFrictionWithLoad").value_or(0.0);
+}
+
+void Climber::extendOuter() {
+    mOuterHookMotor1.Set(config.extendSpeed);
+    mOuterHookMotor2.Set(config.extendSpeed);
+}
+
+void Climber::retractOuter() {
+    mOuterHookMotor1.Set(config.retractSpeed);
+    mOuterHookMotor2.Set(config.retractSpeed);
+}
+
+void Climber::extendInner() {
+    mInnerHookMotor1.Set(config.extendSpeed);
+    mInnerHookMotor2.Set(config.extendSpeed);
+}
+
+void Climber::retractInner() {
+    mOuterHookMotor1.Set(config.retractSpeed);
+    mOuterHookMotor2.Set(config.retractSpeed);
+}
+
+void Climber::lockArms() {
+    mStopServo1.set(config.lockServoPosition);
+    mStopServo2.set(config.lockServoPosition);
+}
+
+void Climber::updateRelay(bool on) {
+    if (on) {
+        mBackDriveRelay.set(frc::Relay.Value.kOn);
+    } else {
+        mBackDriveRelay.set(frc::Relay.Value.kOff);
+    }
+}
+
+double Climber::getInnerArmRotations() { // returns average of the hook positions
+    return (mInnerHook1Encoder.GetPosition() + mInnerHook2Encoder.GetPosition())/2;
+}
+
+double Climber::getOuterArmRotations() { // returns average of the hook positions
+    return (mOuterHook1Encoder.GetPosition() + mOuterHook2Encoder.GetPosition())/2;
+}
--- src/main/include/climber/climber.h
+++ src/main/include/climber/climber.h
@@ -1,11 +1,53 @@
 #pragma once
 
-#include "frc2/command/SubsystemBase.h"
+#include <frc2/command/SubsystemBase.h>
+#include <rev/CANSparkMax.h>
+#include <rev/SparkMaxRelativeEncoder.h>
+#include "constants/interfaces.h"
+#include <cpptoml.h>
 
 class Climber : public frc2::SubsystemBase{
     public:
-        Climber(); // constructor
+        Climber(std::shared_ptr<cpptoml::table> toml); // constructor
+
+        void extendOuter();
+        void retractOuter();
+        void extendInner();
+        void retractInner();
+        void lockArms();
+        void updateRelay();
+
+        double getInnerArmRotations();
+        double getOuterArmRotations();
 
     private:
-        //generally logic things that are not useful to have visable to the rest of the world
+        const int kCountsPerRevolution = 42;
+
+        rev::CANSparkMax mInnerHookMotor1 {interfaces::kInnerArm1, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+        rev::CANSparkMax mInnerHookMotor2 {interfaces::kInnerArm2, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+        rev::CANSparkMax mOuterHookMotor1 {interfaces::kOuterArm1, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+        rev::CANSparkMax mOuterHookMotor2 {interfaces::kOuterArm2, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+
+        rev::SparkMaxRelativeEncoder mInnerHook1Encoder = mInnerHookMotor1.getEncoder();
+        rev::SparkMaxRelativeEncoder mInnerHook2Encoder = mInnerHookMotor2.getEncoder();
+        rev::SparkMaxRelativeEncoder mOuterHook1Encoder = mOuterHookMotor1.getEncoder();
+        rev::SparkMaxRelativeEncoder mOuterHook2Encoder = mOuterHookMotor2.getEncoder();
+
+        frc::Servo mStopServo1 {interfaces::kStopServo1};
+        frc::Servo mStopServo2 {interfaces::kStopServo2};
+        frc::Servo mInnerServo1 {interfaces::kInnerServo1};
+        frc::Servo mInnerServo2 {interfaces::kInnerServo2};
+        frc::Servo mOuterServo1 {interfaces::kOuterServo1};
+        frc::Servo mOuterServo2 {interfaces::kOuterServo2};
+
+        frc::Relay mBackDriveRelay {interfaces::kBackDriveRelay};
+
+        struct {
+            double extendSpeed, retractSpeed;
+            double lockServoPosition;
+            struct {
+                double innerStaticFriction, outerStaticFriction;
+                double innerStaticFrictionWithLoad, outerStaticFrictionWithLoad;
+            } friction;
+        } config;
 };
