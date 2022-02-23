c3de646b5c4d0582769505a6f46c2b964db5a021
PhyXTGears <phyxtgearspro@gmail.com>
2022-02-22 20:44:03 -0500

    Remove extend/retract intake commands
    Because the intake will no longer be retractable, and will not be extended by motors/servos.
    This change was necessary to free fuses for other things.
    
    - Alex G.

--- src/main/include/intake/intake.h
+++ src/main/include/intake/intake.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
-#include <frc/Servo.h>
 #include <rev/CANSparkMax.h>
-#include <frc/AnalogPotentiometer.h>
 
 #include "cpptoml.h"
 #include "constants/interfaces.h"
@@ -12,30 +10,13 @@ class Intake : public frc2::SubsystemBase {
 public:
     Intake (std::shared_ptr<cpptoml::table> toml);
 
-    void extendIntake ();
-    void retractIntake ();
-
     void runRollers ();
     void stopRollers ();
 
-    bool isIntakeExtended ();
-
-    void setStationary (bool isExtended); // sets mCurrentIntakeStatus to stationary
-
 private:
-    enum mIntakeMovementStatus {EXTENDING, RETRACTING, STATIONARY};
-    mIntakeMovementStatus mCurrentIntakeStatus = STATIONARY;
-
     rev::CANSparkMax mRollerMotor {interfaces::kRollerMotor, rev::CANSparkMaxLowLevel::MotorType::kBrushless}; // the motor that turns the rollers to pull the ball in
 
-    frc::Servo mDeployServo1 {interfaces::kDeployServo1};
-    frc::Servo mDeployServo2 {interfaces::kDeployServo2};
-
-    bool mIsIntakeExtended = false;
-
     struct {
-        double rollerSpeed, extendTargetSpeed, retractTargetSpeed;
-        double intakeRetractedPosition, intakeExtendedPosition;
+        double rollerSpeed;
     } config;
-    
 };
