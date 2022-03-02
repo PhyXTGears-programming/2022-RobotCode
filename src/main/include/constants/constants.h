f47f87bc01427337b660744f2459a8c126b4a3c8
nickquednow <56178732+nickquednow@users.noreply.github.com>
2022-03-02 01:12:49 -0500

    fix robot dimentions and max speed

--- src/main/include/constants/constants.h
+++ src/main/include/constants/constants.h
@@ -2,18 +2,21 @@
 
 #include "frc2/command/SubsystemBase.h"
 
+#define FEET_TO_METERS(feet) (feet * 0.3048)
+
+
 namespace constants
 {
 
     //global constants
 
     //robot constraints for swerve in meters
-    const double wheelBase = 20; //currently arbitrary
-    const double trackWidth = 20; //currently arbitrary
+    const double kWheelBase = FEET_TO_METERS(20/12); //20 in wheelbase square
+    const double kTrackWidth = FEET_TO_METERS(20/12);
     // pythagorean theorem of the wheelbase and track width to find hypotenuse
-    const double diameter = sqrt(pow(wheelBase, 2) + pow(trackWidth, 2));
+    const double kDiameter = sqrt(pow(kWheelBase, 2) + pow(kTrackWidth, 2));
 
 
-    const double MaxWheelSpeed = 20; //meters per sec
+    const double kMaxWheelSpeed = 4.5; //meters per sec
 
 }
