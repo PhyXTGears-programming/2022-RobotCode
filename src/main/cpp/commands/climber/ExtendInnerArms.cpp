c4e56e99dd265b159eebcbd5055451eb717612dd
PhyXTGears <phyxtgearspro@gmail.com>
2022-03-04 07:22:49 -0500

    Remove unused code
    - Justin C

--- src/main/cpp/commands/climber/ExtendInnerArms.cpp
+++ src/main/cpp/commands/climber/ExtendInnerArms.cpp
@@ -1,7 +1,5 @@
 #include "commands/climber/ExtendInnerArms.h"
 
-const double kAcceptableExtensionError = 0.05;
-
 ExtendInnerArmsCommand::ExtendInnerArmsCommand(Climber * climber, double targetExtension) {
     AddRequirements(climber);
     mClimber = climber;
