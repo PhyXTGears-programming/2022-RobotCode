91c7df81febbe2c25ae26cc73b39d4fe4b65f5b4
nickquednow <56178732+nickquednow@users.noreply.github.com>
2022-03-08 21:24:39 -0500

    change function to fmin
    done because of compiler errors in simulations

--- src/main/cpp/climber/lerp.cpp
+++ src/main/cpp/climber/lerp.cpp
@@ -3,5 +3,5 @@
 #include <cmath>
 
 double lerp(double min, double max, double value) {
-    return std::min(min + (max - min) * value, max); // makes sure the returned value never exceeds max
+    return std::fmin(min + (max - min) * value, max); // makes sure the returned value never exceeds max
 }
