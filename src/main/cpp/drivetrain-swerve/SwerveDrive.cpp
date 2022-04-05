e17d11a71af926816d2e0cdb17ab4a3bc2f6054d
PhyXTGears <phyxtgearspro@gmail.com>
2022-04-05 19:33:00 -0400

    Use getHeading
    - Elizabeth B.

--- src/main/cpp/drivetrain-swerve/SwerveDrive.cpp
+++ src/main/cpp/drivetrain-swerve/SwerveDrive.cpp
@@ -24,7 +24,7 @@ void SwerveDrive::Periodic()
     frc::SmartDashboard::PutNumber("Steer BR Abs", brWheel.getAbsAngle() / M_PI * 180);
     frc::SmartDashboard::PutNumber("Steer BL Abs", blWheel.getAbsAngle() / M_PI * 180);
 
-    frc::SmartDashboard::PutNumber("Gyro Angle", gyro->GetAngle());
+    frc::SmartDashboard::PutNumber("Gyro Angle", getHeading());
 }
 
 void SwerveDrive::synchronizeTurnEncoders()
